ab09347975e5bc66e89c29d27d9a8aa0d0706421
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2018-08-29 17:05:05 +0300

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('uu-app-root h1')).getText();
+  }
+}
