e31d1b153c73fcc2e8ea2c0f142a113189d1440e
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2018-10-22 14:23:57 +0300

    test commit

--- src/app/third-homework/third-homework.component.ts
+++ src/app/third-homework/third-homework.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GetReposService } from './services/get-repos.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { catchError, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
     selector: 'uu-app-third-homework',
@@ -23,20 +23,23 @@ export class ThirdHomeworkComponent implements OnInit {
                 .pipe(
                     debounceTime(300),
                     distinctUntilChanged(),
-                    switchMap((value: string) =>  this._gitHubReposService.getRepos(value)),
+                    switchMap((value: string) =>  {
+                        return this._gitHubReposService.getRepos(value).pipe(
+                            catchError((error: any) => {
+                                this._gitHubReposService.lastRequestError = error.message;
+                                return of([]);
+                            })
+                        );
+                    }),
                 )
             .subscribe((repos: IGitHubRepo[]) => {
                 this.repositories = repos;
-            }, (error: any) => {
-                this._gitHubReposService.lastRequestError = error.message;
-                this.repositories = [];
             });
     }
 
     public getErrorText(): string {
         return this._gitHubReposService.lastRequestError;
     }
-
     public hideError(): void {
         this._gitHubReposService.lastRequestError = null;
     }
